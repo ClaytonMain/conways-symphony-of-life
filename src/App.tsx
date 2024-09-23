0a052e9393355b171c4ebe64a3f246adb3437a85
ClaytonMain <main.clayton@gmail.com>
2024-09-23 11:57:44 -0600

    Add vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {
     Stats,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { enableMapSet } from "immer";
 import { Suspense, useState } from "react";
 import * as THREE from "three";
@@ -56,6 +57,7 @@ function App() {
 
     return (
         <>
+            <SpeedInsights />
             <DetectClick />
             <Canvas
                 orthographic
